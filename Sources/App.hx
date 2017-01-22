388e9c58887a0e89b10dc7399b9aedf7ffa0f6c0
jefvel <akselkornesjo@gmail.com>
2017-01-22 02:32:16 +0100

    Added simple audio thing

--- Sources/App.hx
+++ Sources/App.hx
@@ -1,7 +1,15 @@
 package;
 
 class App {
+	var sound: kha.Sound;
 	public function new() {
-		
+		kha.Assets.loadEverything(function(){
+			sound = kha.Assets.sounds.select;
+			kha.input.Mouse.get().notify(mDown, null, null, null);
+		});
+	}
+	
+	function mDown(button:Int, x:Int, y:Int) {
+		kha.audio1.Audio.play(sound);
 	}
 }
---
+++ build-windows.bat
@@ -0,0 +1,1 @@
+node kha/make windows
